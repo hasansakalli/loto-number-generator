3ceb953e80b54189e4686f187956531236d12586
HASAN SAKALLI <89208116+hasansakalli@users.noreply.github.com>
2022-05-28 19:38:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,22 +4,20 @@ const liElement = document.querySelectorAll("li");
 function myFunction() {
   myNumber = [];
   for (i = 0; i < 6; i++) {
-    let sayi = Math.floor(Math.random() * liElement.length);
+    let sayi = Math.floor(Math.random() * liElement.length+1);
 
     if (myNumber.indexOf(sayi) == -1) {
       myNumber.push(sayi);
     } else {
-      let sayi = Math.floor(Math.random() * liElement.length);
-      myNumber.push(sayi);
+       i--
     }
     result.innerHTML = myNumber;
   }
 
   liElement.forEach(farbe);
-
   function farbe(item, index) {
     if ((item.style.backgroundColor = "blue")) {
-      for (let j = 0; j < liElement.length - 1; j++) {
+      for (let j = 0; j < liElement.length-1 ; j++) {
         if (myNumber[j] == item.textContent) {
           item.style.backgroundColor = "red";
         }
